639e41b95df097f287f3c73654fcf7ad0a5dbe6b
Scott <scott@immauss.com>
2023-04-16 22:12:41 +0000

    corrected index increase

--- slackex2html.sh
+++ slackex2html.sh
@@ -5,7 +5,9 @@
 # start at 0 index and got to < $length
 # use jq to extract each element of indexed item and assign to bash var
 # output HTML using generated vars
-# take a nap !
+
+# Issues: 
+#	Links are stuck in "<>" which breaks them in the html
 Debug() {
 	if [ $DEBUG -eq 1 ]; then
 		echo -e "debug: $1"
@@ -16,12 +18,15 @@ INPUT="$1"
 RECORDS=$(jq '.|length' $INPUT)
 Debug "INPUT $INPUT RECORDS $RECORDS"
 INDEX=0
+echo "<html><body><table>"
 while [ $INDEX -lt $RECORDS ]; do 
 	TS=$( date -d @$( jq -r ".[$INDEX]|.ts" $INPUT) +%T)
 	USER=$( jq -r ".[$INDEX]|.user_profile.real_name" $INPUT)
 	MSG=$( jq -r ".[$INDEX]|.text" $INPUT)
-	INDEX=$( expr $INDEX + 1)
 	AVATAR=$( jq -r ".[$INDEX]|.user_profile.image_72" $INPUT)
 	Debug "$TS\n$USER\n$MSG\n$AVATAR"
 	echo "<tr><td><b>$TS</b></td><td><img src="$AVATAR"></td><td>${USER}:</td><td> $MSG</td></tr>"
+
+	INDEX=$( expr $INDEX + 1)
 done
+echo "</table></body></html>"
