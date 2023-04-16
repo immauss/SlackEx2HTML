abcd623fb97c95afbec55f52a11a27bb2308d77c
GE Scott Knauss <scott@immauss.com>
2023-04-16 22:59:28 +0200

    added test file

--- slackex2html.sh
+++ slackex2html.sh
@@ -7,10 +7,17 @@
 # output HTML using generated vars
 # take a nap !
 Debug() {
-	if [ $DEBUG ]; then
-		echo "debug: $@"
+	if [ $DEBUG -eq 1 ]; then
+		echo "debug: $1"
+		fi
 	}
 DEBUG="1"
 INPUT="$1"
-RECORDS=$(jq '.|length $INPUT)
+RECORDS=$(jq '.|length' $INPUT)
 Debug "INPUT $INPUT RECORDS $RECORDS"
+INDEX=0
+while [ $INDEX -lt $RECORDS ]; do 
+		TS=$( jq -r ".[$INDEX]|.ts" $INPUT)
+		INDEX=$( expr $INDEX + 1)
+		Debug $TS
+done
