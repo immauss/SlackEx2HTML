10b54df61f48521cc8ddf0df422ec88f3dd08029
Scott <scott@immauss.com>
2023-04-17 13:01:14 +0000

    added support for MacOS version of date.

--- slackex2html.sh
+++ slackex2html.sh
@@ -8,6 +8,17 @@
 
 # Issues: 
 #	Links are stuck in "<>" which breaks them in the html
+OS=$(uname)
+if [ "$OS" == "Darwin" ]; then
+	Date() {
+		date -r $( jq -r ".[$INDEX]|.ts" $INPUT) +%T
+		}
+else
+	Date() {
+		date -d @$( jq -r ".[$INDEX]|.ts" $INPUT) +%T
+		}
+fi
+
 Debug() {
 	if [ $DEBUG -eq 1 ]; then
 		echo -e "debug: $1"
@@ -18,9 +29,10 @@ INPUT="$1"
 RECORDS=$(jq '.|length' $INPUT)
 Debug "INPUT $INPUT RECORDS $RECORDS"
 INDEX=0
-echo "<html><body><table>"
+TITLE=$( echo $INPUT | sed "s/.json$//")
+echo "<html><body><h1>$TITLE</h1><table>"
 while [ $INDEX -lt $RECORDS ]; do 
-	TS=$( date -d @$( jq -r ".[$INDEX]|.ts" $INPUT) +%T)
+	TS=$( Date )
 	USER=$( jq -r ".[$INDEX]|.user_profile.real_name" $INPUT)
 	MSG=$( jq -r ".[$INDEX]|.text" $INPUT)
 	AVATAR=$( jq -r ".[$INDEX]|.user_profile.image_72" $INPUT)
