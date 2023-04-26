5315344034e807699ddd3e3d8d1bbd1a325d2bf0
GE Scott Knauss <scott@immauss.com>
2023-04-26 12:32:13 +0200

    MVP .... It seems to work and meet all initial requirements.

--- slackex2html.sh
+++ slackex2html.sh
@@ -16,7 +16,7 @@
 
 # This for the user to configure.
 DEBUG="0"
-SRCDIR="/Users/scott/Downloads/CatalystSlackExport/" # Set this if you dont' want to use the current working directory.
+SRCDIR="/Users/scott/Downloads/CatalystSlackExport" # Set this if you dont' want to use the current working directory.
 DSTDIR="/Users/scott/SlackTest"
 mkdir -p $DSTDIR
 # This is where we start. It should be the root of the archive from Slack.
@@ -53,16 +53,12 @@ else
 		date -d @$( jq -r ".[$INDEX]|.ts" $INPUT) +%T
 		}
 fi
-
 # Setup a debug function..
 Debug() {
 	if [ $DEBUG -eq 1 ]; then
 		echo -e "debug:\n$1" | tee -a  $SRCDIR/slackex2html.debug
 		fi
 	}
-
-
-
 # First we need to create the destination structure.
 cd $SRCDIR
 SRCDIRS=$(find . -mindepth 1 -type d | sed "s/^\.\///" | sort )
@@ -72,13 +68,12 @@ cd $DSTDIR
 for DIR in $SRCDIRS; do 
 	mkdir -p $DIR 2> /dev/null
 done
-
 # Create an associative array for the user name to ID mapping.
 Debug " $SRCDIR/users.json"
 URECORDS=$( jq ".|length" $SRCDIR/users.json)
 INDEX=0
 declare -A Users
-echo "Reading Users in to array for replacements later"
+echo "Reading Users into array for replacements later"
 while [ $INDEX -lt $URECORDS ]; do
         ID=$( jq -r ".[$INDEX].id" $SRCDIR/users.json )
         NAME=$( jq -r ".[$INDEX].real_name" $SRCDIR/users.json )
@@ -86,20 +81,17 @@ while [ $INDEX -lt $URECORDS ]; do
         INDEX=$( expr $INDEX + 1 )
         Users[$ID]="$NAME"
 done
-
 # Process each file. The final dir structure will mirror the archive's structure, 
 # but include sub folders for the year under each channel folder. 
 # Files in each folder will be by month. 
 #
 echo "Now processing Channels"
 for DIR in $SRCDIRS; do 
-	echo "Channel #$DIR"
-	
+	echo -e "\nChannel #$DIR"
 	cd ${SRCDIR}/${DIR}
 	Debug "PWD $(pwd)"
 	for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12; do	
 		Debug "MONTH $MONTH"		
-
 		for INPUT in $( ls ????-$MONTH-??.json 2> /dev/null| sort); do
 			Debug "INPUT at Start -$INPUT-"
 			DATE=$( echo $INPUT | sed "s/.json$//")
@@ -108,6 +100,8 @@ for DIR in $SRCDIRS; do
 			OUTPUT="${DSTDIR}/${DIR}/${YEAR}/${MONTH}.html"
 			if ! [ -f "$OUTPUT" ]; then
 				echo "<html><body>" > $OUTPUT
+				echo "<center><h1>#$DIR</h1></center>" >> $OUTPUT
+				echo "xxxNAVIGATIONxxx" >> $OUTPUT
 			fi
 			Debug "MONTH $MONTH\n YEAR $YEAR\n OUTPUT $OUTPUT\n PWD $(pwd)"
 			# Setup the initial Table.
@@ -118,6 +112,9 @@ for DIR in $SRCDIRS; do
 			INDEX=0
 			Debug "OUTPUT $OUTPUT\n SRCDIR $SRCDIR\n INPUT $INPUT\n DATE $DATE\n YEAR $YEAR\n PWD $(pwd)"
 			while [ $INDEX -lt $RECORDS ]; do 
+				if [ $(expr $INDEX % 100) -eq 0 ]; then 
+					echo -n "."
+				fi
 				TS=$( Date )
 				MSG=$( jq -r ".[$INDEX]|.text" $INPUT | tr '<|>|\r' ' ')
 # 				if  echo $MSG | grep -isq http ; then
@@ -127,46 +124,46 @@ for DIR in $SRCDIRS; do
 # 					MSG=$( echo $MSG | tr '<|>' ' ')
 # 				fi
 				# Replace the UIDs with actual user names on mentions
-				if echo "$MSG" | egrep '<|>' ; then
-					echo "$MSG"
-					exit
-				fi 
-				while echo "$MSG" | egrep -sq "U[A-Z0-9]{10}\b" ; do 
+
+				while echo "$MSG" | tr -d "\n" | egrep -sq "[@\b]U[A-Z0-9]{10}\b" ; do 
 					
 					ID=$(echo "$MSG" | tr -d "\n" | sed -E "s/^.*(U[A-Z0-9]{10}).*$/\1/")
 					if [ "${Users[$ID]}x" != "x" ]; then
-						Debug "ID: $ID User: \"${Users[$ID]}\""
-						Debug "MSG: $MSG"
+						Debug "ID: $ID User: \"${Users[$ID]}\"\nMSG: $MSG"
 						MSG=$(echo "$MSG" | sed "s/${ID}/${Users[$ID]}/g")
 						if [ $? -ne 0 ]; then
 							Debug "MSG: $MSG\nID: $ID\nUser: \"${Users[$ID]}\""
 						fi
 					else 
-						if [ ${#ID} -lt 11 ]; then
+						if [ ${#ID} -eq 11 ]; then
 							MSG=$(echo "$MSG" | sed "s/${ID}/UNKNOWN/g")
+							Debug "##NO User Match ##\nMSG: \"$MSG\"\nID: $ID\nUser: ${Users[$ID]}"
 						fi
 						if [ $? -ne 0 ]; then
-							Debug "##NO User Match ##\nMSG: \"$MSG\"\nID: $ID\nUser: ${Users[$ID]}"
+							Debug "##NO User Match ##\nsed command failed.\nMSG: \"$MSG\"\nID: $ID\nUser: ${Users[$ID]}"
 						fi
 					fi	
 					Debug "Found $ID in MSG:\n$MSG\nFrom: $INPUT at INDEX: $INDEX"
 				done 
-
-
 				# This sets up the Username and avatar, but sets them to null if the last
 				# entry was from the same user. This gives a cleaner look in the rendered html
 				USER=$( jq -r ".[$INDEX]|.user_profile.real_name" $INPUT)
+				Debug "LAST $LASTUSER Current $USER USERNAME $USERNAME"
 				if [ "$LASTUSER" != "$USER" ]; then		
-					USERNAME=$USER
-					AVATAR=$( jq -r ".[$INDEX]|.user_profile.image_72" $INPUT)
+					USERNAME="$USER"
+					AVATAR="<img src=\"$( jq -r ".[$INDEX]|.user_profile.image_72" $INPUT)\">"
 				else
 					USERNAME=""
 					AVATAR=""
 				fi
 				LASTUSER=$USER
 				Debug "$TS\n$USER\n$MSG\n$AVATAR"
+				if [ "$USERNAME" == "null" ]; then
+					USERNAME=""
+					AVATAR=""
+				fi
 				# Create a new row in the table;
-				echo "<tr><td style=\"vertical-align: top\"><b>${USERNAME}</b></td><td style=\"vertical-align: top\"><img src=\"$AVATAR\"></td><td style=\"vertical-align: top\">$TS:</td><td style=\"vertical-align: top\"> $MSG</td></tr>" >> $OUTPUT
+				echo "<tr><td style=\"vertical-align: top\"><b>${USERNAME}</b></td><td style=\"vertical-align: top\">$AVATAR</td><td style=\"vertical-align: top\">$TS:</td><td style=\"vertical-align: top\"> $MSG</td></tr>" >> $OUTPUT
 				# increase the INDEX
 				INDEX=$( expr $INDEX + 1)
 			done 
@@ -179,8 +176,13 @@ for DIR in $SRCDIRS; do
 		# We first check to see if the file exists as it may not.
 		# This happens when there is no source file for the months in this loop.
 		if [ -f "$OUTPUT" ]; then
-			echo "</body></html>" >> $OUTPUT
+			if [ $( grep -c xxxNAVIGATIONxxx $OUTPUT) -lt 2 ]; then 
+				echo "xxxNAVIGATIONxxx" >> $OUTPUT
+				echo "</body></html>" >> $OUTPUT
+			fi
 		fi
 	done
 
 done
+# just to make the screen output prettier.
+echo
