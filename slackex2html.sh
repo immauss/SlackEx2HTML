ac4a39d9049bab65cd0363caffc9da97df49a22b
GE Scott Knauss <scott@immauss.com>
2023-04-27 22:10:42 +0200

    Migrated finalize into main script

--- slackex2html.sh
+++ slackex2html.sh
@@ -5,19 +5,22 @@
 # start at 0 index and got to < $length
 # use jq to extract each element of indexed item and assign to bash var
 # output HTML using generated vars
-
-
 # Check message for UID pattern <U..........>. Use sed.. Then set variable for user with AWK against 
 # cleaned up list of users to store actual user's real name in variable. Then use sed again 
 # to replace it in the message. 
-# To then repeat this until there are no longer any user IDs in the message. Maybe this 
-# Should be a function?  
-# Use " while ! grep UID.Patter msg; do" for iteration ...
 
-# This for the user to configure.
+# Things for the user to configure.
 DEBUG="0"
-SRCDIR="/Users/scott/Downloads/CatalystSlackExport" # Set this if you dont' want to use the current working directory.
+SRCDIR="/Users/scott/Downloads/CatalystSlackExport/Test" # Set this if you dont' want to use the current working directory.
 DSTDIR="/Users/scott/SlackTest"
+# RELDIR is the directory on the server where you are storing the files as seen from the web.
+# Example: /SlackHistory 
+RELDIR="/resources/Site/SlackHistory"
+# Main site home page
+HOME="https://vcisocatalyst.org"
+# Name of Organization for Title pages
+ORGNAME="vCISO Catalyst"
+
 mkdir -p $DSTDIR
 # This is where we start. It should be the root of the archive from Slack.
 if [ -z $SRCDIR ]; then
@@ -74,13 +77,19 @@ URECORDS=$( jq ".|length" $SRCDIR/users.json)
 INDEX=0
 declare -A Users
 echo "Reading Users into array for replacements later"
+USERCOUNT=0
 while [ $INDEX -lt $URECORDS ]; do
         ID=$( jq -r ".[$INDEX].id" $SRCDIR/users.json )
         NAME=$( jq -r ".[$INDEX].real_name" $SRCDIR/users.json )
         Debug "$ID,$NAME"
         INDEX=$( expr $INDEX + 1 )
         Users[$ID]="$NAME"
+		USERCOUNT=$( expr $USERCOUNT + 1 )
+		if [ $(expr $USERCOUNT % 10) -eq 0 ]; then
+			echo -n "."
+		fi
 done
+echo
 # Process each file. The final dir structure will mirror the archive's structure, 
 # but include sub folders for the year under each channel folder. 
 # Files in each folder will be by month. 
@@ -117,12 +126,7 @@ for DIR in $SRCDIRS; do
 				fi
 				TS=$( Date )
 				MSG=$( jq -r ".[$INDEX]|.text" $INPUT | tr '<|>|\r' ' ')
-# 				if  echo $MSG | grep -isq http ; then
-# 					Debug "$MSG\nTHERE'S a LINK IN THIS MSG!!!"
-# 					#remove the < & > characters so the link will show up.
-# 					# this should be fixed to make this an anchor tag.
-# 					MSG=$( echo $MSG | tr '<|>' ' ')
-# 				fi
+
 				# Replace the UIDs with actual user names on mentions
 
 				while echo "$MSG" | tr -d "\n" | egrep -sq "[@\b]U[A-Z0-9]{10}\b" ; do 
@@ -186,3 +190,94 @@ for DIR in $SRCDIRS; do
 done
 # just to make the screen output prettier.
 echo
+
+# Now we need to create some index files to make the generated HTML easier to navigate.
+# Starts at Index of channels
+# In each channel folder, there is an index wich lists:
+#  YEAR:
+#     Jan Feb Mar
+#	  Apr May Jun
+#     Jul Aug Sep
+#     Oct Nov Dec
+# Each month links to that months messages
+# at top and bottom of each page, navigation to prev & next in
+# chronological order.
+
+
+# Array for Month names
+# Element 0 is NULL because there is no Month 0, this make the element number align with the 
+# numerical month.
+MONTHA=("NULL" "Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec") 
+
+# Create Channel index.
+echo "<html><body>" > index.html
+echo "<h1>$ORGNAME Slack History</h1>" >> index.html
+for Channel in $( ls -d -1 */ | tr -d "/"); do
+	echo "<a href=\"$Channel/index.html\">#$Channel</a><br>" >> index.html
+done
+echo "</body></html>" >> index.html
+
+# Process through channels 
+cd $DSTDIR
+for DIR in $(ls -d -1 */); do
+
+	cd $DSTDIR/$DIR
+	echo "Working in $(pwd)"
+	Channel=$( echo $DIR | tr -d "/")
+	# First create the Navigation from prev <-> Next. 
+	files=($(find . -name "*.html"  | grep -v index.html |sort -n))
+		for (( i=0; i<${#files[@]}; i++ )); do
+			filename="${files[$i]}";
+			if (( $i > 0 )); then
+				prevfile="${files[$i-1]}"
+				PREV="<a href=\"$RELDIR/$DIR/$prevfile\">Previous</a>"
+			else
+				PREV=""
+			fi
+			if (( $i < ${#files[@]}-1 )); then
+				nextfile="${files[$i+1]}"
+				NEXT="<a href=\"$RELDIR/$DIR/$nextfile\">Next</a>"
+			else
+				NEXT=""
+			fi
+			# Insert Navigation
+			NAVIGATION="<a href=\"$HOME\">Home</a></br><a href=\"$RELDIR/index.html\">Channel Index</a> </p>/$PREV --- $NEXT</br>"
+
+			sed -i "" "s|xxxNAVIGATIONxxx|$NAVIGATION|g" $filename
+		done
+		# Now create the channel index file based on available month files.
+		# filenames look like: "./2023/04.html"
+		echo "<html><body>" > index.html
+		echo "<center><h2>Index of <i>#$Channel</i> at $ORGNAME</h2></center>" >> index.html
+		echo "<a href=\"$HOME\">Home</a></br><a href=\"$RELDIR/index.html\">Channel Index</a>" >> index.html
+		declare -A Links
+		for (( i=0; i<${#files[@]}; i++ )); do
+			filename="${files[$i]}";
+			YEAR=$( echo $filename | sed "s/^\.\/\(....\)\/..\.html$/\1/")
+			YEARS=(${YEARS[@]} "$YEAR")
+			MONTH=${MONTHA[$(echo $filename | sed "s/^\.\/....\/\(..\)\.html$/\1/;s/^0//")]}
+			Links[$YEAR-$MONTH]="<a href=\"$filename\">$MONTH</a>"
+		done
+		# Make YEARS array unique so we can use it for table creation.
+		YEARS=($(for x in "${YEARS[@]}"; do echo "${x}"; done | sort -ru))
+		for Year in ${YEARS[@]}; do 
+			
+			for Month in ${MONTHA[@]}; do
+				#echo "$DIR $Year $Month ${Links[$Year-$Month]}"
+				if [ "x${Links[$Year-$Month]}" == "x" ]; then
+					Links[$Year-$Month]="$Month"
+				fi
+			done
+			echo "<h3>$Year</h3>" >> index.html
+			echo "<table border="1">" >> index.html
+			echo "<tr><td>${Links[${Year}-Jan]}</td><td>${Links[${Year}-Feb]}</td><td>${Links[${Year}-Mar]}</td></tr>" >> index.html
+			echo "<tr><td>${Links[${Year}-Apr]}</td><td>${Links[${Year}-May]}</td><td>${Links[${Year}-Jun]}</td></tr>" >> index.html
+			echo "<tr><td>${Links[${Year}-Jul]}</td><td>${Links[${Year}-Aug]}</td><td>${Links[${Year}-Sep]}</td></tr>" >> index.html
+			echo "<tr><td>${Links[${Year}-Oct]}</td><td>${Links[${Year}-Nov]}</td><td>${Links[${Year}-Dec]}</td></tr>" >> index.html
+			echo "</table>" >> index.html			
+		done 
+		echo "</body></html>"  >> index.html
+		# Make sure these are clean for next run
+		YEARS=()
+		Links=()
+done
