80d2d7786a396c2289eb8adb1700236d3dcd9300
GE Scott Knauss <scott@immauss.com>
2023-06-07 10:07:32 +0200

    search changes that will likely be destroyed with lunr ....

--- search/setup-search.sh
+++ search/setup-search.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # The gpgcheck is disable here because for some reason, Elastic is still using SHA1 signatures and 
-# RHEL9/Rocky9 want sha2 .... There's probably another answer to this problem. 
+# RHEL9/Rocky9 want sha2 .... There's probably another answer to this problem. One that looks at the 
+# Linux release and decides how to handle it based on that.
 #
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 cat << EOF > /etc/yum.repos.d/elastic.repo
@@ -24,10 +25,9 @@ autorefresh=1
 type=rpm-md
 EOF
 # Install Kibana and elasticsearch
-dnf -y  install --enablerepo=elasticsearch elasticsearch kibana
+dnf -y  install --enablerepo=elasticsearch elasticsearch kibana jq java-11-openjdk
 
-# Need to edit/copy elasticsearch.yml here:
-# TODO
+copy elasticsearch.yml /etc/elasticsearch/
 
 # start elastic search
 systemctl enable elasticsearch --now 
@@ -47,4 +47,14 @@ KENROLL=$(/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s 
 # ssh -L 5601:localhost:5601 <server hostname> 
 firewall-cmd --add-port 5601/tcp
 
+#elastic enterprise search 
+
+
+
+# Display Cluster Info and password
+curl -k -u "elastic:${ELSUPASS}" https://localhost:9200  | jq .
+echo "Elastic Superuser password: $ELSUPASS"
+
+# Elastic UI 
+# https://docs.elastic.co/search-ui/tutorials/elasticsearch
 
